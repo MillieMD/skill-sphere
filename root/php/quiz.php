9fb1257e2e3d4be0a7a9143a28fb4ee05f64fa60
Usmaan-Nadeem <89669935+Usmaan-Nadeem@users.noreply.github.com>
2024-02-12 11:44:16 +0000

    Add files via upload

--- root/php/quiz.php
+++ root/php/quiz.php
@@ -19,28 +19,39 @@
     $conn->query($sql);
  }
 
- function createQuestion($quizId, $questionText, $conn) {
-    $sql = "INSERT INTO questions (quiz_id, question_text) 
-    VALUES ($quizId, '$questionText')";
+ function getQuizIdFromName($quizName, $conn) {
+    $sql = "SELECT quiz_id FROM quiz WHERE quiz_name = '$quizName' ";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    return $row["quiz_id"];
+ }
+
+ function createQuestion($quizId, $questionText, $questionType, $conn) {
+    $sql = "INSERT INTO questions (quiz_id, question_text, question_type)
+    VALUES ($quizId, '$questionText', '$questionType')";
     $conn->query($sql);
  }
 
 function createAnswer ($questionId, $answerText, $conn) {
     $sql = "INSERT INTO answers (question_id, answer_text)
     VALUES ($questionId, '$answerText')";
-    $conn->query($sql);
+    return $conn->query($sql);
 }
 
 /*  This creates a question and uses its question_id as a foreign key for its answer
     use this method to create questions and answers 
     because it ensures that a question points to its answer
 */
-function createQuestionAndAnswer($quizId, $questionText, $answerText, $conn) {
-    createQuestion($quizId, $questionText, $conn);
+function createQuestionAndAnswer($quizId, $questionType, $questionText, $answerText, $conn) {
+    createQuestion($quizId, $questionText, $questionType, $conn);
     $last_id = $conn->insert_id;
     createAnswer($last_id, $answerText, $conn);
     $last_idAnswer = $conn->insert_id;
     $sql = "UPDATE questions SET answer_id = $last_idAnswer WHERE question_id = $last_id";
     $conn->query($sql);
 }
+
+echo getQuizIdFromName("quiz", $conn);
+createQuestionAndAnswer(getQuizIdFromName("quiz", $conn), "Multiple Choice", "Test question2", "Test answer2", $conn);
+
 ?>
