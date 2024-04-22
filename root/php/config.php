f5db12ab738e7c6679e08704fa16f078e6d1d97c
MillieMD <asdapricesavers@gmail.com>
2024-04-22 12:55:16 +0100

    Update config.php

--- root/php/config.php
+++ root/php/config.php
@@ -1,8 +1,10 @@
 <?php
 
+    // Removed log in details from public repo, add in as you're using
+    // Do not commit your password and username
     $host = "localhost";
-    $username = "u2265397";
-    $password = "JK22feb04jk";
-    $database = "u2265397";
+    $username = "";
+    $password = "";
+    $database = "";
 
 ?>
